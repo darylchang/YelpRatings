bda074a88ad9a2009ccccfd66dde2c43a3d3a92b
Daryl Chang <darylc123@gmail.com>
2015-05-26 13:12:10 -0700

    Fixing eval code

--- evaluate.py
+++ evaluate.py
@@ -2,17 +2,28 @@ from sklearn.metrics import mean_absolute_error
 import numpy as np
 
 def crossEntropy(yTrue, yPred):
+    print yPred
     return -np.sum(yTrue * np.log(yPred))
 
+def makeOneHot(label, dim):
+    a = np.zeros(dim)
+    a[label] = 1
+    return a
+
 def evaluateDev(classifier, reviews, x, y, trainPercentage=70, devPercentage=15):
+    # Get dimensions
+    M = x.shape[0]
+    N = x.shape[1]
+    numClasses = len(np.unique(y))
+    
     # Set train-dev-test split indices
-    N = x.shape[0]
-    trainLower, trainUpper = 0, int(trainPercentage * N / 100.)
-    devLower, devUpper = trainUpper, trainUpper + int(devPercentage * N / 100.) 
+    trainLower, trainUpper = 0, int(trainPercentage * M / 100.)
+    devLower, devUpper = trainUpper, trainUpper + int(devPercentage * M / 100.) 
     
     xTrain, yTrain = x[trainLower:trainUpper], y[trainLower:trainUpper]
     xDev, yDev = x[devLower:devUpper], y[devLower:devUpper]
     classifier.fit(xTrain, yTrain)
-    yPred = classifier.predict(xDev)
-    print "Cross Entropy on dev set: {}".format(crossEntropy(yDev, yPred))
+    yPred = classifier.predict_proba(xDev)
+    yTrue = [makeOneHot(label-1,numClasses) for label in yDev]
+    print "Cross Entropy on dev set: {}".format(crossEntropy(yTrue, yPred))
     
