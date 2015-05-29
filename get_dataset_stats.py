efff8e52c6ae1c33c51f398f0403626d7146cf1b
April Yu <yu.april.s@gmail.com>
2015-05-29 16:42:48 -0700

    parse neg and pos words

--- get_dataset_stats.py
+++ get_dataset_stats.py
@@ -1,21 +1,29 @@
 import json
 from collections import Counter
-from nltk.corpus import sentiwordnet as swn
-
+from sets import Set
 
 stars = Counter()
-lex = {}
+positive = Set()
+negative = Set()
 
-stars[1] += 1
+with open('lexicon/negative-words.txt') as neg:
+	for l in neg:
+		if l[0] == ';':
+			continue
+		negative.add(l.strip('\n'))
 
-
+with open('lexicon/positive-words.txt') as neg:
+	for l in neg:
+		if l[0] == ';':
+			continue
+		positive.add(l.strip('\n'))
 
 # with open('data/yelp_academic_dataset_review.json') as data_file:    
 #     for line in data_file:
 #     	data = json.loads(line)
 #     	stars[data["stars"]] += 1
 
-with open('stats/stars.txt', 'w+') as f:
-	f.write("Counts for star ratings in reviews\n")
-	for k,v in stars.most_common():
-		f.write("Rating {}: {}\n".format(k,v))
+# with open('stats/stars.txt', 'w+') as f:
+# 	f.write("Counts for star ratings in reviews\n")
+# 	for k,v in stars.most_common():
+# 		f.write("Rating {}: {}\n".format(k,v))
