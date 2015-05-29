9913ae390de71a1a230ab089d3ca6057b998f1f2
Daryl Chang <darylc123@gmail.com>
2015-05-29 16:40:28 -0700

    Adding code for sentiment distributions

--- get_dataset_stats.py
+++ get_dataset_stats.py
@@ -13,5 +13,19 @@ def getStarDist():
 		for k,v in stars.most_common():
 			f.write("Rating {}: {}\n".format(k,v))
 
+def getSentiment():
+	posWordDist = Counter()
+	negWordDist = Counter()
+	with open('data/yelp_academic_dataset_review.json') as data_file:    
+	    for index, line in enumerate(data_file):
+	    	if index % 100000 == 0:
+	    		print index
+	    	data = json.loads(line)
+	    	numPosWords = 
+	    	numNegWords = 
+	    	posWordDist[data["stars"]] = 
+	    	negWordDist[data["stars"]] = 
+
+
 if __name__=='__main__':
 	getStarDist()
