59463828ae5dfac55af63a898950a0b1864e1283
Daryl Chang <darylc123@gmail.com>
2015-05-29 16:34:52 -0700

    Got star distribution

--- get_dataset_stats.py
+++ get_dataset_stats.py
@@ -1,21 +1,17 @@
 import json
 from collections import Counter
-from nltk.corpus import sentiwordnet as swn
 
+def getStarDist():
+	stars = Counter()
+	with open('data/yelp_academic_dataset_review.json') as data_file:    
+	    for index, line in enumerate(data_file):
+	    	data = json.loads(line)
+	    	stars[data["stars"]] += 1
 
-stars = Counter()
-lex = {}
+	with open('stats/stars.txt', 'w+') as f:
+		f.write("Counts for star ratings in reviews\n")
+		for k,v in stars.most_common():
+			f.write("Rating {}: {}\n".format(k,v))
 
-stars[1] += 1
-
-
-
-# with open('data/yelp_academic_dataset_review.json') as data_file:    
-#     for line in data_file:
-#     	data = json.loads(line)
-#     	stars[data["stars"]] += 1
-
-with open('stats/stars.txt', 'w+') as f:
-	f.write("Counts for star ratings in reviews\n")
-	for k,v in stars.most_common():
-		f.write("Rating {}: {}\n".format(k,v))
+if __name__=='__main__':
+	getStarDist()
