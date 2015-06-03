37c94d0f16b26dd9e38af80e327f5cc86afeac11
April Yu <yu.april.s@gmail.com>
2015-06-02 20:38:49 -0700

    implemented RNTN - now parsing Yelp data into tree structure

--- assignment3/run.sh
+++ assignment3/run.sh
@@ -7,14 +7,14 @@ set -x
 ###################
 
 # training params
-epochs=6
-step=5e-2
+epochs=97
+step=1e-2
 wvecDim=30
 
 # for RNN2 only, otherwise doesnt matter
 middleDim=45
 
-model="RNN2" #either RNN, RNN2, RNN3, RNTN, or DCNN
+model="RNTN" #either RNN, RNN2, RNN3, RNTN, or DCNN
 
 
 ######################################################## 
@@ -29,7 +29,12 @@ fi
 
 echo $outfile
 
+#command to run from random initialization
+# python runNNet.py --step $step --epochs $epochs --outFile $outfile \
+#                 --middleDim $middleDim --outputDim 5 --wvecDim $wvecDim --model $model 
 
-python runNNet.py --step $step --epochs $epochs --outFile $outfile \
-                --middleDim $middleDim --outputDim 5 --wvecDim $wvecDim --model $model 
-
+#command to run from pre-trained and train on Yelp data?
+infile="models/${model}_wvecDim_${wvecDim}_step_${step}_2.bin"
+outfile="models/${model}_wvecDim_${wvecDim}_step_${step}_2_Yelp.bin"
+python runYelp.py --step $step --epochs $epochs --infile $infile --outFile $outfile2 \
+				--middleDim $middleDim --outputDim 5 --wvecDim $wvecDim --model $model
